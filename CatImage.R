258e2bcfc844f15aba04d1c187576263a2119f8d
Jonathan Troiano <jjtroiano0272@2256mc13.local>
2019-04-29 17:52:06 -0400

    Currently stuck halfway through step 6. I continue to get a 'nonconformable arguments' error when trying to take the dot product of the 3x3 submatrices and Xmask but...I find out that this is because the dimensions of the submatrices I'm selecting are...somehow null.

--- CatImage.R
+++ CatImage.R
@@ -1,5 +1,8 @@
-M <- read.csv("./Cat.csv", header = FALSE)
+M_raw <- read.csv("./Cat.csv", header = FALSE)
+M <- as.matrix(read.csv("./Cat.csv", header = FALSE))
 d <- dim(M)
+d[1] # Number of rows
+d[2] # Number of columns
 #as.raster(M) # Then you use plot() to plot this data.
 
 # Check-step to verify matrix contains no negative values
@@ -10,7 +13,7 @@ if (sum(M >= 0))
 
 # (4) Defining Xmask, Ymask
 Xmask <- matrix(c(-1, 0, 1, -2, 0, 2, -1, 0, 1), nrow = 3, ncol = 3, byrow = TRUE)
-Ymask <- t(Xmask) #A
+Ymask <- t(Xmask)
 
 # TODO: U[2, 2] must be variable indices, M[1:3] must alos be variable indices.
 # Fills a matrix as big as M but with zeroes.
@@ -18,31 +21,35 @@ U <- M * 0
 # CAUTION! You're doing type conversions here and that may cause unstable data.
 U[2, 2] <- sum(as.matrix(M[1:3, 1:3]) %*% Xmask)
 
-#Keeps a 3x3 matrix throughout in selections
-row <- 1
-col <- 1
-# Successfully moves through matrix entirely.
-# TODO: Move through matrix in 3x3 block operations
 # I believe U will go UP UNTIL ncol(M)-2 and save it to U[ncol(M)-2]
-for (row in 1:nrow(M)) {
-  for (col in 1:ncol(M)) {
-    print(paste("Row", row, " Column ", col))
-    # TODO: U[2, 2] <- sum(as.matrix(M[1:3, 1:3]) %*% Xmask)
-    # Selecting 3x3 blocks
-    # Make sure it has an upper limit
-    M[row:row+2, col:col+2]
+# TODO: The error comes about because the first element is -1 (because we substract 2)
+for (row in 1 : (d[1]-2) ) {
+  
+  for (col in 1 : (d[2]-2) ) {
+    # Possible IF dim check
+    if ( dim(M[row:row+2, col:col+2])[1] == dim(Xmask)[2] ) {
+      M[row:row+2, col:col+2] %*% Xmask
+    }
+  }
+}
+
+
+
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Test Materials
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+M_test <- matrix(1:36, nrow = 6, ncol = 6, byrow = TRUE)
+U_test <- M_test * 0
+
+for (row in 1 : (nrow(M_test) - 2) ) {
+  for (col in 1 : (ncol(M_test) - 2) ) {
+    # TODO: Assigning to U....
+    # sum() returns a single value from an mxn matrix
+    sum(M_test[row:row+2, col:col+2] %*% Xmask) # This selects a 3x3 matrix and sums its product with Xmask
   } 
 }
 
-# Test Materials
-############
-U_test <- M_test * 0
-for (row in 1:nrow(M_test)) {
-  for (col in 1:ncol(M_test)) {
-    # TODO: U[2, 2] <- sum(as.matrix(M[1:3, 1:3]) %*% Xmask)
-    # Selecting 3x3 blocks
-    # Make sure it has an upper limit
-    M[row:row+2, col:col+2]
-  } 
-}
-M_test <- matrix(1:36, nrow = 6, ncol = 6, byrow = TRUE)
+# TODO: U goes from U[2, 2] to U[nrow(U) - 1, ncol(U) - 1]
